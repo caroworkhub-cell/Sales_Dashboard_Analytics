6621427e30abcb1a83305f3ba3991cb1eed01fc9
Caroline <caroworkhub@gmail.com>
2026-03-06 16:43:29 +0300

    Add SQL assignment script with 50 queries

---
+++ Script.sql
@@ -0,0 +1,12 @@
+--create a database(hr_db)
+--> Create a schema(trainers) 
+--> create table(DE_trainers, DS_trainers)
+--SQL commands
+--creating a database
+--create database database_name;
+
+create database hr_data;
+ show search_path;
+set search_path to
+create database kenyaretails;
+ create schema 
